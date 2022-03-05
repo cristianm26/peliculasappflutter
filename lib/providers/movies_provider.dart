cf53bf9938f4cd5e3e2e5cd64318ef9aff815e1b
Cristian Munoz <cristian.munoz@twiinshrm.com>
2022-03-04 21:41:31 -0500

    App de Peliculas Flutter

---
+++ lib/providers/movies_provider.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:peliculas/helpers/debouncer.dart';
+import 'package:peliculas/models/models.dart';
+
+class MoviesProvider extends ChangeNotifier {
+  final String _baseURL = "api.themoviedb.org";
+  final String _apiKey = 'ed74f5139f387c7a0d9544673fc58fac';
+  final String _language = 'es-ES';
+
+  List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
+  Map<int, List<Cast>> movieCast = {};
+
+  int _popularPage = 0;
+  final debouncer = Debouncer(
+    duration: const Duration(milliseconds: 500),
+  );
+
+  final StreamController<List<Movie>> _suggestionStreamController =
+      StreamController.broadcast();
+  Stream<List<Movie>> get suggestionStream =>
+      _suggestionStreamController.stream;
+
+  MoviesProvider() {
+    // print('Movies Provider Inicializado');
+    getOnDisplayMovies();
+    getPopularMovies();
+  }
+  Future<String> _getJsonData(String endpoint, [int page = 1]) async {
+    final url = Uri.https(_baseURL, endpoint, {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '$page',
+    });
+
+    final response = await http.get(url);
+    return response.body;
+  }
+
+  getOnDisplayMovies() async {
+    final jsonData = await _getJsonData('3/movie/now_playing');
+
+    final nowPlayingResponse = NowPlayingResponse.fromJson(jsonData);
+
+    /* print(nowPlayingResponse.results[0].title); */
+    onDisplayMovies = nowPlayingResponse.results;
+    notifyListeners();
+  }
+
+  getPopularMovies() async {
+    _popularPage++;
+    final jsonData = await _getJsonData('3/movie/popular', _popularPage);
+
+    final popularResponse = PopularResponse.fromJson(jsonData);
+
+    /* print(nowPlayingResponse.results[0].title); */
+    popularMovies = [...popularMovies, ...popularResponse.results];
+    notifyListeners();
+  }
+
+  Future<List<Cast>> getMovieCast(int movieId) async {
+    //revisar el Mapa
+    if (movieCast.containsKey(movieId)) return movieCast[movieId]!;
+
+    final jsonData = await _getJsonData('3/movie/$movieId/credits');
+
+    final creditsResponse = CreditsResponse.fromJson(jsonData);
+
+    movieCast[movieId] = creditsResponse.cast;
+    return creditsResponse.cast;
+  }
+
+  Future<List<Movie>> searchMovies(String query) async {
+    final url = Uri.https(_baseURL, '3/search/movie', {
+      'api_key': _apiKey,
+      'language': _language,
+      'query': query,
+    });
+
+    final response = await http.get(url);
+    final searchResponse = SearchResponse.fromJson(response.body);
+    return searchResponse.results;
+  }
+
+  void getSuggestionsByQuery(String query) {
+    debouncer.value = '';
+    debouncer.onValue = (value) async {
+      // print('Tenemos valor a buscar: $value');
+      final results = await searchMovies(value);
+      _suggestionStreamController.add(results);
+    };
+
+    final timer = Timer.periodic(const Duration(milliseconds: 300), (_) {
+      debouncer.value = query;
+    });
+
+    Future.delayed(const Duration(milliseconds: 301))
+        .then((_) => timer.cancel());
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
+import 'package:peliculas/search/search_delegate.dart';
+import 'package:peliculas/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Peliculas en Cine'),
+          centerTitle: true,
+          elevation: 0,
+          actions: [
+            IconButton(
+              onPressed: () => showSearch(context: context, delegate: MovieSearchDelegate()),
+              icon: const Icon(Icons.search_outlined),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              //CardSwipers
+              CardSwiper(
+                movies: moviesProvider.onDisplayMovies,
+              ),
+              // Listado Horizontal de peliculas
+              MovieSlider(
+                movies: moviesProvider.popularMovies,
+                title: 'Peliculas Populares',
+                onNextPage: () => moviesProvider.getPopularMovies(),
+              ),
+            ],
+          ),
+        ));
+  }
+}
